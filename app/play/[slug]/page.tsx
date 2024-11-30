aadccc7624266f7b8fdb5028784614bf14721123
Kha <53520595+irzko@users.noreply.github.com>
2024-11-30 11:39:31 +0000

    .

--- app/play/[slug]/page.tsx
+++ app/play/[slug]/page.tsx
@@ -18,7 +18,7 @@ export default async function Page({
 }: {
   params: Promise<{ slug: string }>;
 }) {
-  const slug = (await params).slug;
+  const slug = decodeURIComponent((await params).slug);
   if (!slug) {
     return null;
   }
